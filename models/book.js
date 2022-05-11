f2f79178b3fe31f1f47d5dc76206608de6cea3c5
shinbian11 <shinbian11@gmail.com>
2022-05-11 14:32:13 +0900

    Schema의 속성 => default 값 지정하는 법

--- models/book.js
+++ models/book.js
@@ -4,8 +4,17 @@ const Schema = mongoose.Schema;
 const book = new Schema({
   bookname: String,
   auther: String,
-  price: Number,
+  price: {
+    // price 값이 들어오지 않았을 때, default로 5000을 저장해줌 (null 값도 들어오면 안됨)
+    type: Number,
+    default: 5000,
+  },
   publish: Date,
+  sales: {
+    // sales 값이 들어오지 않았을 때, default로 true을 저장해줌 (null 값도 들어오면 안됨)
+    type: Boolean,
+    default: true,
+  },
 });
 
 const bookData = mongoose.model("bookinfo", book);

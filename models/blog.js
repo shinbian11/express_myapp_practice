04dc3ce80bdd8c029e662a61c2eee54ab07d2660
shinbian11 <shinbian11@gmail.com>
2022-05-16 15:00:32 +0900

    auto increment 적용하기

--- models/blog.js
+++ models/blog.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+const autoIncrement = require("mongoose-auto-increment");
 const blogSchema = mongoose.Schema;
 
 // 블로그 글 제목, 본 글 내용
+// auto increment
+
+autoIncrement.initialize(mongoose); // 초기화 (필수)
 
 const blog = new blogSchema(
   {
@@ -13,11 +17,19 @@ const blog = new blogSchema(
       type: String,
       required: true,
     },
+    no: Number,
   },
   {
     timestamps: true,
   }
 );
+
+blog.plugin(autoIncrement.plugin, {
+  model: "blog", // blog 모델
+  field: "no", // autoincrement를 적용할 field
+  startAt: 20,
+  increment: 1,
+});
 
 const blogModel = mongoose.model("blog", blog);
 module.exports = blogModel;
